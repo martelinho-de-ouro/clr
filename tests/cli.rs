8eba022f9e4faf50d3516eed14f5ddf77823f633
Helio Frota <00hf11@gmail.com>
2024-02-18 01:09:52 -0300

    chore: update

--- tests/cli.rs
+++ tests/cli.rs
@@ -9,14 +9,14 @@ fn runs() {
     cmd.assert().success().stdout("Hello, world!\n");
 }
 
-// #[test]
-// fn true_ok() {
-//     let mut cmd = Command::cargo_bin("true").unwrap();
-//     cmd.assert().success();
-// }
+#[test]
+fn true_ok() {
+    let mut cmd = Command::cargo_bin("true").unwrap();
+    cmd.assert().success();
+}
 
-// #[test]
-// fn false_not_ok() {
-//     let mut cmd = Command::cargo_bin("false").unwrap();
-//     cmd.assert().failure();
-// }
+#[test]
+fn false_not_ok() {
+    let mut cmd = Command::cargo_bin("false").unwrap();
+    cmd.assert().failure();
+}

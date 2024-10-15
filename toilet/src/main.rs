93e70e71f23455c69113939691df2421a2ad54cf
Helio Frota <00hf11@gmail.com>
2024-10-14 22:37:50 -0300

    toilet refactor

--- toilet/src/main.rs
+++ toilet/src/main.rs
@@ -1,7 +1,7 @@
 use std::{fs::File, io::BufReader};
 
 use anyhow::Result;
-use rodio::{source::Source, Decoder, OutputStream};
+use rodio::{source::Source, Decoder, OutputStream, OutputStreamHandle};
 
 enum Intensity {
     Small(String),
@@ -9,27 +9,24 @@ enum Intensity {
     Big(String),
 }
 
+fn play_sound(
+    path: &str,
+    duration: u64,
+    stream_handle: &OutputStreamHandle,
+) -> Result<(), anyhow::Error> {
+    let file = BufReader::new(File::open(path)?);
+    let source = Decoder::new(file)?;
+    stream_handle.play_raw(source.convert_samples())?;
+    std::thread::sleep(std::time::Duration::from_secs(duration));
+    Ok(())
+}
+
 fn flush(intensity: Intensity) -> Result<(), anyhow::Error> {
     let (_stream, stream_handle) = OutputStream::try_default()?;
     match intensity {
-        Intensity::Small(i) => {
-            let file = BufReader::new(File::open(i)?);
-            let source = Decoder::new(file)?;
-            stream_handle.play_raw(source.convert_samples())?;
-            std::thread::sleep(std::time::Duration::from_secs(3));
-        }
-        Intensity::Medium(i) => {
-            let file = BufReader::new(File::open(i)?);
-            let source = Decoder::new(file)?;
-            stream_handle.play_raw(source.convert_samples())?;
-            std::thread::sleep(std::time::Duration::from_secs(4));
-        }
-        Intensity::Big(i) => {
-            let file = BufReader::new(File::open(i)?);
-            let source = Decoder::new(file)?;
-            stream_handle.play_raw(source.convert_samples())?;
-            std::thread::sleep(std::time::Duration::from_secs(9));
-        }
+        Intensity::Small(i) => play_sound(&i, 3, &stream_handle)?,
+        Intensity::Medium(i) => play_sound(&i, 4, &stream_handle)?,
+        Intensity::Big(i) => play_sound(&i, 9, &stream_handle)?,
     }
     Ok(())
 }
